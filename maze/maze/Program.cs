54bd09712711de16e2e44997c3d6143e9ec05cfe
Fede Fomin <wffomin@protonmail.com>
2022-02-21 06:50:36 +0100

    Uploading main solution with three public classes World class which will contain the levels, Player class which will be used to create our player and finally the GameLoop class which will be used to have the game logic in it.

---
+++ maze/maze/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Welcome to the maze game.");
+
+            ReadKey(true);
+        }
+    }
+}
